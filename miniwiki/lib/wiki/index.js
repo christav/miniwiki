d5944fb2d10679113572e2bfd9157add8da38048
Chris Tavares <ctavares@microsoft.com>
2012-07-19 16:28:03 -0700

    Building out wiki parser

--- miniwiki/lib/wiki/index.js
+++ miniwiki/lib/wiki/index.js
@@ -1,3 +1,19 @@
+// Function library for dealing with wikitext
+// to html formatting
+
 (function () {
-	
+	var _ = require("underscore");
+
+	exports.parse = function wikiParse(wikiText) {
+		return new PlainTextNode(wikiText);
+	};
+
+	// Parse tree result types
+
+	// Plain text objects
+	function PlainTextNode(plainText) {
+		return {
+			render: function () { return plainText; }
+		}
+	}
 })();
--- miniwiki/tests/tests.js
+++ miniwiki/tests/tests.js
@@ -1,10 +1,26 @@
+var wiki = require('./../lib/wiki');
 var assert = require('should');
 
 describe("Libraries", function () {
-	describe("Wiki library", function () {
-		it("should be present", function () {
-			require("./../lib/wiki").should.be.a('object');
-		});
-	});
+    describe("Wiki library", function () {
+        it("should be present", function () {
+            wiki.should.be.a('object');
+        });
+    });
 });
 
+describe("Wiki Markup", function () {
+    describe("plain text", function () {
+        it("should return a text node", function () {
+            var result = wiki.parse("plain text");
+            result.should.be.a("object");
+            result.should.have.property("render");
+        });
+
+        it("should render as itself", function () {
+            var source = "some plain text";
+            wiki.parse(source).render().should.equal(source);
+        });
+
+    });
+})
